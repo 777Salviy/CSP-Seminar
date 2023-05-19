b3f31212a368fceeefa515e5cffbd2f2396672c6
777salviy@gmail.com <777salviy@gmail.com>
2023-05-19 15:53:44 +0300

    add sem1 task05 solution

--- sem1/task05/Program.cs
+++ sem1/task05/Program.cs
@@ -2,3 +2,12 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 // 2 -> " -2, -1, 0, 1, 2"
 
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = -N;
+
+while  (negativeN <= N)
+{
+Console.Write( negativeN + ", ");
+negativeN += 1;
+}
