2b995592074bd207b9ea7cb6718f04f91117b9c8
777salviy@gmail.com <777salviy@gmail.com>
2023-05-19 14:41:10 +0300

    add task02 solution

--- sem1/task02/Program.cs
+++ sem1/task02/Program.cs
@@ -5,3 +5,19 @@
 // a = 9, b = -3->да 
 // a = -3 b = 9 -> нет
 
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber) // Равно ли 25 числу 5 в квадрате?
+{
+    // Да, истина
+    Console.WriteLine("Число " + firstNumber + " является квадратом числа " + secondNumber);
+}
+else // Нет
+{
+    Console.WriteLine("Число " + firstNumber + " НЕ является квадратом числа " + secondNumber);
+}
+
