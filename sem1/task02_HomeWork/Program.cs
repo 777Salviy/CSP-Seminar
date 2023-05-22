436db419116cbf9f246011c378c0264f697dc9bc
777salviy@gmail.com <777salviy@gmail.com>
2023-05-22 13:53:56 +0300

    add task02 Home Work correction

--- sem1/task02_HomeWork/Program.cs
+++ sem1/task02_HomeWork/Program.cs
@@ -6,7 +6,7 @@
 Console.WriteLine("Введите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите ыторое число: ");
+Console.WriteLine("Введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 int max = firstNumber;
