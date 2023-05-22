d0a10ba0dad0b31d784d5a388dd3b400b68dff69
777salviy@gmail.com <777salviy@gmail.com>
2023-05-22 17:36:50 +0300

    task08 Home Work solution

--- sem1/task08_HomeWork/Program.cs
+++ sem1/task08_HomeWork/Program.cs
@@ -2,4 +2,12 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
+int count = 2;
+while (count <= N)
+{
+    Console.Write(count + ", ");
+    count += 2;
+}
