78aaae9c7d098a854566b2dff1e6f6fa2c2010db
777salviy@gmail.com <777salviy@gmail.com>
2023-05-22 15:59:30 +0300

    task06 Home Work solution

--- sem1/task06_HomeWork/Program.cs
+++ sem1/task06_HomeWork/Program.cs
@@ -4,4 +4,21 @@
 // - 3->нет
 // 7->нет
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if (number == 0)
+{
+    Console.WriteLine("Нельзя делить на ноль, хотя по общим правилам математики 0 относится к четным числам");
+}
+else
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Число является четным");
+    }
+    else
+    {
+        Console.WriteLine("Число является нечетным");
+    }
+}
