37f264ffba2db8fa0aa5131dc73d31bab476d435
777salviy@gmail.com <777salviy@gmail.com>
2023-05-23 21:01:32 +0300

    task10 Home Work solution

--- sem2/task10_HomeWork/Program.cs
+++ sem2/task10_HomeWork/Program.cs
@@ -3,4 +3,16 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.WriteLine("Введите трехзначное число: ");
+int Number = int.Parse(Console.ReadLine());
 
+if (Number < 1000 && Number > 99)
+{
+    int a = Number / 10;
+    int b = a % 10;
+    Console.WriteLine("Вторая цифра в числе " + Number + " -> " + b);
+}
+else
+{
+    Console.WriteLine("Введенное число не трехзначное");
+}
