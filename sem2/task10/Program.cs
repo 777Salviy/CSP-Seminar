42b92620ff4eb4dac3c5c9faafdac3c5dd64c6fa
777salviy@gmail.com <777salviy@gmail.com>
2023-05-23 14:29:36 +0300

    task10 solution VOID Random + FOR

--- sem2/task10/Program.cs
+++ sem2/task10/Program.cs
@@ -1,2 +1,43 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
 
+void GenNumbers(int[] arr)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(10, 100);
+    }
+}
+
+// Random rnd = new Random();
+// int rndNum = rnd.Next(10, 100); // 10 - 99
+//                                 // 5 6
+// int a = rndNum % 10; // 4
+// int b = rndNum / 10;
+// if (a > b)
+// {
+//     Console.WriteLine(a);
+// }
+// else
+// {
+//     Console.WriteLine(b);
+// }
+
+int[] arr = new int[10]; // { 0,0,0,0,0,0....}
+
+GenNumbers(arr); // { 15,15,64,16,83....}
+
+for (int i = 0; i < arr.Length; i++) // 0 = 15
+{
+    int left = arr[i] / 10; // 15 / 10 = 1
+    int right = arr[i] % 10; // 15 % 10 = 5
+    if (left > right)
+    {
+        Console.WriteLine($"Наибольшая цифра числа {arr[i]} = " + left);
+    }
+    else
+    {
+        Console.WriteLine($"Наибольшая цифра числа {arr[i]} = " + right);
+    }
+}
