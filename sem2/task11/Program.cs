fdeae32ef8146dd2348bccd79a7d92ab757c2e5f
777salviy@gmail.com <777salviy@gmail.com>
2023-05-23 14:43:35 +0300

    add task11 solution

--- sem2/task11/Program.cs
+++ sem2/task11/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Задача 11. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
 
 
+Random rnd = new Random();
+int rndNum = rnd.Next(100, 1000);
+Console.WriteLine(rndNum);
+
+int a = rndNum / 100;
+int c = rndNum % 10;
+int res = 10 * a + c;
+
+Console.WriteLine(res);
