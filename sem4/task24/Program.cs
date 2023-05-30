36aad6b76f713ef12873ff8b8eb889472d7d8c98
777salviy@gmail.com <777salviy@gmail.com>
2023-05-30 14:05:36 +0300

    task24 solution Method "for"

--- sem4/task24/Program.cs
+++ sem4/task24/Program.cs
@@ -3,4 +3,16 @@
 // 4 -> 10
 // 8 -> 36
 
+Console.Write("Введите число А: ");
+
+int limit = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0; // Изначально суммы нет
+// for (точка входа; условие цикла; увел.счетчика)
+for (int i = 1; i <= limit; i++)
+{
+    sum += i; // sum = sum + i
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {limit} = {sum}");
 
