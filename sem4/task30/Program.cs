dac786937e72cd14f241b4b9f7edc23161401c93
777salviy@gmail.com <777salviy@gmail.com>
2023-05-30 15:07:49 +0300

    task 30 solution

--- sem4/task30/Program.cs
+++ sem4/task30/Program.cs
@@ -2,3 +2,10 @@
 // заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+int size = 8;
+int[] array = new int[size];
+for (int i = 0; i < size;i++)
+{
+    array[i] = new Random().Next(5);
+}
+Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
