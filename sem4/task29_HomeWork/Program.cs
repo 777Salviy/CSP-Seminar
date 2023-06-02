2f9ab2ef8ba0fab0b6f4ebf1671909337c8f0bdf
777salviy@gmail.com <777salviy@gmail.com>
2023-06-02 23:30:34 +0300

    task29 solution

--- sem4/task29_HomeWork/Program.cs
+++ sem4/task29_HomeWork/Program.cs
@@ -3,3 +3,28 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33-> [6, 1, 33]
 
+void GetArray(int[] newRow)
+{
+    int lenth = newRow.Length;
+    int index = 0;
+    while (index < lenth)
+    {
+        newRow[index] = new Random().Next(10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(collection[position]);
+        position += 1;
+    }
+}
+
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
