2d6a3d8459c0e141a2685c2ff8f650ef86f7f514
777salviy@gmail.com <777salviy@gmail.com>
2023-06-02 14:50:32 +0300

    add task33 initial + previous task data

---
+++ sem5/task33/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив[6, 7, 19, 345, 3]->нет
+// - 3; массив[6, 7, 19, 345, 3]->да
+
+int[] GetArray(int size, int minValue, int maxValue)
+// size = 12, minValue = -9, maxValue = 9
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++) // i < resultArray.Length = i < size
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
