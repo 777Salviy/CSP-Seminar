e221af6149d47b1c54a85ffebad72c6eaecfc5ef
777salviy@gmail.com <777salviy@gmail.com>
2023-06-02 13:01:02 +0300

    task25 Home Work exeist mistake, need find correct code

--- sem4/task25_Homework/Program.cs
+++ sem4/task25_Homework/Program.cs
@@ -3,3 +3,17 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4-> 16
 
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа: ");
+int numberPower = Convert.ToInt32(Console.ReadLine());
+
+int powerCount = 0;
+
+while (powerCount <= numberPower)
+{
+    numberA *= numberA;
+    powerCount++;
+}
+Console.WriteLine($"{numberA}");
