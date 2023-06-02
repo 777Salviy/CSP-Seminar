c23b0d4be14a55686f0c2b82ec42a4dc07739c1a
777salviy@gmail.com <777salviy@gmail.com>
2023-06-02 15:00:17 +0300

    task33 solution

--- sem5/task33/Program.cs
+++ sem5/task33/Program.cs
@@ -16,3 +16,27 @@ int[] GetArray(int size, int minValue, int maxValue)
 
 int[] array = GetArray(12, -9, 9);
 Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+
+bool FindElement(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number)
+        {
+            return true; // true - элемент найден
+        }
+    }
+    return false;// false - элемент НЕ найден
+}
+
+int numberForSearch = new Random().Next(11); // [0-10]
+Console.WriteLine($"Рандомное число для поиска: {numberForSearch}");
+if (FindElement(array, numberForSearch)) // FindElement(array,numberForSearch) == true
+{
+    Console.WriteLine($"Число {numberForSearch} в массиве присутствует");
+}
+else // FindElement(array,numberForSearch) == false
+{
+    // $"Текст {переменная} текчст {переменная}"
+    Console.WriteLine($"Число {numberForSearch} в массиве ОТСУТСТВУЕТ");
+}
