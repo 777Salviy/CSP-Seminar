41fadc1343dc355a47bd2452029ddbb479b29d56
777salviy@gmail.com <777salviy@gmail.com>
2023-06-02 14:46:17 +0300

    task32 solution

--- sem5/task32/Program.cs
+++ sem5/task32/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GetArray(int size, int minValue, int maxValue)
+// size = 12, minValue = -9, maxValue = 9
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++) // i < resultArray.Length = i < size
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+int[] ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= (-1); //  arr[i] =  arr[i] * (-1)
+    }
+    return arr;
+}
+
+int[] array = GetArray(12, -9, 9);
+// Интерполяция ($)
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+Console.WriteLine($"Перевернутый массив: [ {String.Join("; ", ReverseArray(array))} ]");
