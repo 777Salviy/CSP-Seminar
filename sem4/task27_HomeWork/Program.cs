22ed34bec19985f53f8b03f5de7db4bc60bef6db
777salviy@gmail.com <777salviy@gmail.com>
2023-06-02 23:00:28 +0300

    task27 solution used 2 methods

--- sem4/task27_HomeWork/Program.cs
+++ sem4/task27_HomeWork/Program.cs
@@ -4,4 +4,35 @@
 // 82 -> 10
 // 9012 -> 12
 
+int NumberOrder(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+int SumNumber(int N, int numOrder)
+{
+    int result = 0;
+    for (int i = 0; i < numOrder; i++)
+    {
+        int digit = N % 10;
+        result = result + digit;
+        N = N / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int numberPosition = NumberOrder(number);
+// Console.WriteLine(numberPosition);
+
+int SUM = SumNumber(number, numberPosition);
+Console.WriteLine($"Сумма цифр числа {number} = {SUM}");
 
