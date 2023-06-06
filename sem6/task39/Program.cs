b608ba6b9f439b5d13d5baa7dbd12c2059be9bb2
777salviy@gmail.com <777salviy@gmail.com>
2023-06-06 13:59:27 +0300

    task39 add GetArray Method (size, min, max)

--- sem6/task39/Program.cs
+++ sem6/task39/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 39: Напишите программу, 
+// которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+int[] resultArray = GetArray(10, 0, 10); // size = 10, min = 0, max = 10
+Console.WriteLine($"Исходный массив: [{String.Join("; ", resultArray)}]");
