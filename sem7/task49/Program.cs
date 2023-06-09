56d07b15a4853804332dba5a9f0234f071cf1d1b
777salviy@gmail.com <777salviy@gmail.com>
2023-06-09 14:49:14 +0300

    add task49 solution

---
+++ sem7/task49/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 49. Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] resMatrix = GetMatrix(rows, columns, -5, 10);
+PrintMatrix(resMatrix);
+
+int[,] NewMatrix(int[,] newArray)
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                newArray[i, j] = (int)Math.Pow(newArray[i, j], 2);
+            }
+        }
+        Console.WriteLine();
+    }
+    return newArray;
+}
+
+int[,] evenNumberMatrix = NewMatrix(resMatrix);
+PrintMatrix(evenNumberMatrix);
