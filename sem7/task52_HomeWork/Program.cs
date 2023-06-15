ed40bf308929b3cd165b51980626831ca2c1c60d
777salviy@gmail.com <777salviy@gmail.com>
2023-06-15 20:36:56 +0300

    task52 HW add Fill Matrix Method

--- sem7/task52_HomeWork/Program.cs
+++ sem7/task52_HomeWork/Program.cs
@@ -6,3 +6,20 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+int[,] FillMatrix(int n, int m, int min, int max)
+{
+    int[,] matrix = new int[n, m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix()
+{
+
+}
