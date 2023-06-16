a1b87c14767816952716b148ad4dfa9ccb01bc9a
777salviy@gmail.com <777salviy@gmail.com>
2023-06-16 14:20:33 +0300

    add sem 9 example finish

--- sem9/sem_example/Program.cs
+++ sem9/sem_example/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Рекурсия. факториал.
 
-
+int Factorial(int value)
+{
+    // Базовый случай - случай выхода из рекурсии, бесконечный вызов
+    if (value == 1) return 1;
+    // Рекурсивный случай - задание рекурсии
+    return value * Factorial(value - 1);
+}
+int N = 4;
+Console.WriteLine($"Факториал от числа {N} = {Factorial(N)}");
