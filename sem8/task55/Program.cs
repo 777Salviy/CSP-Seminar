224ed17c797752eb34b0b1ad1f4e2656774510c2
777salviy@gmail.com <777salviy@gmail.com>
2023-06-26 08:59:19 +0300

    add task55 solution

--- sem8/task55/Program.cs
+++ sem8/task55/Program.cs
@@ -3,4 +3,40 @@
 // В случае, если это невозможно, 
 // программа должна вывести сообщение для пользователя.
 
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-20, 20);
+}
 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeMass(int[,] array)
+{
+    int[,] array1 = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            //      (array[i, j], array1[j, i]) = (array1[j, i], array[i, j]);
+            array1[j, i] = array[i, j];
+    return array1;
+}
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(ChangeMass(array));
