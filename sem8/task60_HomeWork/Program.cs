f428e77066b857c21ce16a8125a027e869f66f7c
777salviy@gmail.com <777salviy@gmail.com>
2023-06-26 09:26:49 +0300

    add task60_HW solution

--- sem8/task60_HomeWork/Program.cs
+++ sem8/task60_HomeWork/Program.cs
@@ -7,4 +7,68 @@
 //  27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+int sizeX = InputInt("Введите размерность X: ");
+int sizaY = InputInt("Введите размерность Y: ");
+int sizeZ = InputInt("Введите размерность Z: ");
+int countNumbers = 59;
+
+if (sizeX * sizaY * sizeZ > countNumbers)
+{
+    Console.WriteLine("Результат:");
+    Console.WriteLine("Массив слишком БОЛЬШОЙ!");
+    return;
+}
+
+int[,,] resultNumbers = Create3DArray(sizeX, sizaY, sizeZ);
+
+Console.WriteLine("Результат:");
+
+for (int i = 0; i < resultNumbers.GetLength(0); i++)
+{
+    for (int j = 0; j < resultNumbers.GetLength(1); j++)
+    {
+        for (int k = 0; k < resultNumbers.GetLength(2); k++)
+        {
+            Console.WriteLine($"{resultNumbers[i, j, k]} ({i}, {j}, {k})");
+        }
+    }
+    Console.WriteLine();
+}
+
+int[,,] Create3DArray(int size1, int size2, int size3)
+{
+    int[,,] array = new int[size1, size2, size3];
+    int[] val = new int[countNumbers];
+    int Number = 10;
+    for (int i = 0; i < val.Length; i++)
+        val[i] = Number++;
+
+    for (int i = 0; i < val.Length; i++)
+    {
+        int randomIndex = new Random().Next(0, val.Length);
+        int temp = val[i];
+        val[i] = val[randomIndex];
+        val[randomIndex] = temp;
+    }
+
+    int valueIndex = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = val[valueIndex++];
+            }
+        }
+    }
+    return array;
+}
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return int.Parse(Console.ReadLine());
+}
 
