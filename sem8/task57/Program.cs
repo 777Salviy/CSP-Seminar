6004363aa1ceef043e56853591fb549b5d6bbc46
777salviy@gmail.com <777salviy@gmail.com>
2023-06-26 09:07:39 +0300

    add task57 solution

--- sem8/task57/Program.cs
+++ sem8/task57/Program.cs
@@ -9,4 +9,51 @@
 // 4 встречается 1 раз 
 // 6 встречается 2 раза
 
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+int[] CountMass(int[,] array)
+{
+    int[] array1 = new int[10];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array1[array[i, j]] += 1;
+    return array1;
+}
+
+
+void PrintResult(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            Console.WriteLine($"{i} встречается {array[i]} раза");
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintResult(CountMass(array));
 
